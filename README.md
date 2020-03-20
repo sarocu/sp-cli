23486456dcda66f877c5945d2384337b00cfa553
Sam <sam.currie@simuwatt.com>
2020-03-20 17:26:01 -0600

    adding additional project boilerplate

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # sp-cli
+The Superplus CLI is here to generate boilerplate statistical learning project assets. This includes generating basic project structure, creating new models, making tests and generating visualizations. 
+
+## Create a new project
+```bash
+sp new project
+
+# optionally with a cool name:
+sp new project --name CoolStuff
+```
 
 ## Add to path:
+After building with `cargo build`, add the `sp` executable to the path for use:
 ```bash
 export PATH="~/sp-cli/sp/target/debug:$PATH"
 ```
--- sp/src/main.rs
+++ sp/src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate clap;
 
+#[macro_use]
+extern crate serde_json;
+
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
@@ -13,24 +16,12 @@ use clap_nested::{Command, Commander, MultiCommand};
 use dialoguer::Input;
 use colored:: Colorize;
 
+mod project;
+
 static VERSION: &'static str = "0.1.0";
 static AUTHOR: &'static str = "SAROCU";
 
 fn main() {
-    // let sp_new = Command::new("new")
-    //     .description("Scaffold out a new project, model, test, or data package")
-    //     .options(|app| {
-    //         app.arg(
-    //             Arg::with_name("debug")
-    //                 .short("d")
-    //                 .help("Print out additional detail")
-    //         )
-    //     })
-    //     .runner(|args: &str, matches: &ArgMatches<'_>| {
-    //         let debug = clap::value_t!(matches, "debug", bool).unwrap_or_default();
-    //         Ok(())
-    //     });
-
     let project = Command::new("project")
         .description("Create a new project structure")
         .options(|app| {
@@ -51,10 +42,30 @@ fn main() {
             fs::create_dir(path)?;
             println!("Project Path: {}", PathBuf::from("./").join(project_name).into_os_string().into_string().unwrap().blue());
 
+            println!("{}", "Creating config and entrypoint files...".green());
             let mut config_file = fs::File::create(PathBuf::from("./").join(project_name).join("sp.json"))?;
+            config_file.write_all(project::project_boilerplate::config_text(project_name).as_bytes())?;
 
+            let mut entrypoint = fs::File::create(PathBuf::from("./").join(project_name).join("index.py"))?;
+            entrypoint.write_all(project::project_boilerplate::entrypoint_text().as_bytes());
+            let mut init_file = fs::File::create(PathBuf::from("./").join(project_name).join("__init__.py"))?;
+
+            let mut setup_py = fs::File::create(PathBuf::from("./").join(project_name).join("setup.py"))?;
+            
+            println!("{}", "Creating directory structure...".green());
             let mut model_path = PathBuf::from("./").join(project_name).join("models");
             fs::create_dir(model_path)?;
+
+            let mut test_path = PathBuf::from("./").join(project_name).join("tests");
+            fs::create_dir(test_path)?;
+
+            let mut output_path = PathBuf::from("./").join(project_name).join("output");
+            fs::create_dir(output_path)?;
+
+            let mut data_path = PathBuf::from("./").join(project_name).join("data");
+            fs::create_dir(data_path)?;
+
+            println!("🍻  {} 🍻", "Finished! Boilerplate project created".cyan());
             Ok(())
         });
 
@@ -83,50 +94,4 @@ fn main() {
         })
         .run()
         .unwrap();
-
-
-    // let matches = App::new("Superplus CLI")
-    //     .version(VERSION)
-    //     .author(AUTHOR)
-    //     .about("Project Scaffolding for Scientific and Engineering Projects")
-    //     .arg(Arg::with_name("debug")
-    //         .short("d")
-    //         .long("debug")
-    //         .value_name("DEBUG_SETTING")
-    //         .help("Toggles the debug setting - accepts true/false")
-    //         .takes_value(true))
-    //     .subcommand(SubCommand::with_name("new")
-    //         .about("Generate a new project, model, test, or data model")
-    //         .version(VERSION)
-    //         .author(AUTHOR)
-    //         .subcommand(SubCommand::with_name("project")
-    //             .about("Create a new project directory and boilerplate structure")
-    //             .version(VERSION)
-    //             .author(AUTHOR)
-    //             .arg(Arg::with_name("name")
-    //                 .short("n")
-    //                 .long("name")
-    //                 .value_name("PROJECT_NAME")
-    //                 .help("New Project Name")
-    //                 .takes_value(true))
-    //             )
-    //         ).get_matches();
-
-    // let scaffold_name = matches.value_of("name").unwrap_or("app");
-
-    // if let Some(matches) = matches.subcommand_matches("new") {
-    //     if matches.is_present("project") {
-    //         let mut path = PathBuf::from("/").join(scaffold_name);
-    //         fs::create_dir_all(path);
-    //     }
-    // }
-
-    
-    // if let Some(matches) = matches.subcommand_matches("project") {
-    //     if matches.is_present("name") {
-    //         let mut path = PathBuf::from("/").join(scaffold_name);
-    //         fs::create_dir_all(path);
-    //     }
-    // }
-
 }
